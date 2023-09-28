67405d3b939d49498605bfe3054e366864085a19
soundarya-tech <soundaryagji@gmail.com>
2023-09-28 13:23:37 +0530

    First commit for fetchData

---
+++ src/Table.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import Row from './Row'
+const Table = ({items}) => {
+  return (
+    <div className='table-container'>
+      <table>
+        <tbody>
+            {
+                items.map(item=>{
+                    return(
+                        <Row key={item.id} item={item}/>
+                    )
+                    
+                })
+            }
+        </tbody>
+      </table>
+    </div>
+  )
+}
+
+export default Table
